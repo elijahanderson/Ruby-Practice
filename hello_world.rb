3ff7e717a3cf4cc39b03e85eb069cae2d91b9000
Elijah Anderson <elijahanderson_2019@depauw.edu>
2018-04-05 09:33:45 -0400

    Take Input

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,13 @@
+=begin
+  HELLO WORLD -- my first ruby program
+  (This is a block comment)
+=end
+
+# print out "Hello, World!" (This is a single-line comment)
 puts "Hello, World!"
+
+# print out the user's name in a friendly message
+puts "Please enter your beautiful name: "
+name = gets.chop # .chop removes the last char from the entered string (\n in this case)
+puts "Hi there, #{name}, I'm your computer!"
+puts "Hi there, " + name + ", I'm your computer!"
